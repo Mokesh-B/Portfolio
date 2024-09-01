7ef396e1011997353e679837314151adbcb6c0d5
mokesh <mokeshbalasubramani985@gmail.com>
2024-09-01 08:43:30 -0100

    proj chngs

--- js/script.js
+++ js/script.js
@@ -59,62 +59,62 @@ for (let i = 0; i < totalFilterBtn; i++) {
 
 // Portfolio Lighbox
 
-const lightbox = document.querySelector(".lightbox"),
-  lightboxImg = lightbox.querySelector(".lightbox-img"),
-  lightboxText = lightbox.querySelector(".caption-text"),
-  lightboxClose = lightbox.querySelector(".lightbox-close"),
-  lightboxCounter = lightbox.querySelector(".caption-counter");
+// const lightbox = document.querySelector(".lightbox"),
+//   lightboxImg = lightbox.querySelector(".lightbox-img"),
+//   lightboxText = lightbox.querySelector(".caption-text"),
+//   lightboxClose = lightbox.querySelector(".lightbox-close"),
+//   lightboxCounter = lightbox.querySelector(".caption-counter");
 
-let itemIndex = 0;
+// let itemIndex = 0;
 
-for (let i = 0; i < totalPortfolioItem; i++) {
-  portfolioItems[i].addEventListener("click", function () {
-    itemIndex = i;
-    changeItem();
-    toggleLightbox();
-  });
-}
+// for (let i = 0; i < totalPortfolioItem; i++) {
+//   portfolioItems[i].addEventListener("click", function () {
+//     itemIndex = i;
+//     changeItem();
+//     toggleLightbox();
+//   });
+// }
 
-function toggleLightbox() {
-  lightbox.classList.toggle("open");
-}
+// function toggleLightbox() {
+//   lightbox.classList.toggle("open");
+// }
 
-function changeItem() {
-  let imgSrc = portfolioItems[itemIndex]
-    .querySelector(".portfolio-img img")
-    .getAttribute("src");
-  lightboxImg.src = imgSrc;
-  lightboxText.innerHTML = portfolioItems[itemIndex].querySelector(
-    "h4"
-  ).innerHTML;
-  lightboxCounter.innerHTML = itemIndex + 1 + " of " + totalPortfolioItem;
-}
+// function changeItem() {
+//   let imgSrc = portfolioItems[itemIndex]
+//     .querySelector(".portfolio-img img")
+//     .getAttribute("src");
+//   lightboxImg.src = imgSrc;
+//   lightboxText.innerHTML = portfolioItems[itemIndex].querySelector(
+//     "h4"
+//   ).innerHTML;
+//   lightboxCounter.innerHTML = itemIndex + 1 + " of " + totalPortfolioItem;
+// }
 
-function prevItem() {
-  if (itemIndex === 0) {
-    itemIndex = totalPortfolioItem - 1;
-  } else {
-    itemIndex--;
-  }
-  changeItem();
-}
+// function prevItem() {
+//   if (itemIndex === 0) {
+//     itemIndex = totalPortfolioItem - 1;
+//   } else {
+//     itemIndex--;
+//   }
+//   changeItem();
+// }
 
-function nextItem() {
-  if (itemIndex === totalPortfolioItem - 1) {
-    itemIndex = 0;
-  } else {
-    itemIndex++;
-  }
-  changeItem();
-}
+// function nextItem() {
+//   if (itemIndex === totalPortfolioItem - 1) {
+//     itemIndex = 0;
+//   } else {
+//     itemIndex++;
+//   }
+//   changeItem();
+// }
 
 // close lightbox
 
-lightbox.addEventListener("click", function (event) {
-  if (event.target === lightboxClose || event.target === lightbox) {
-    toggleLightbox();
-  }
-});
+// lightbox.addEventListener("click", function (event) {
+//   if (event.target === lightboxClose || event.target === lightbox) {
+//     toggleLightbox();
+//   }
+// });
 
 // Aside Navbar
 
